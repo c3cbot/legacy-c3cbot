b8017a99a83231e547ca8e0d01c3ef0f531f5a83
lequanglam <29724654+lequanglam@users.noreply.github.com>
2020-02-26 16:48:16 +0700

    Update

--- index.js
+++ index.js
@@ -2,10 +2,40 @@
 /* eslint-disable camelcase */
 var child_process = require("child_process");
 
-var errorCode = 7378278;
-var first = true;
+/**
+ * C3CLoader Function
+ *
+ * @param   {boolean}    first  First time?
+ *
+ * @return  {undefined}         Nothing
+ */
+function loader(first) {
+  if (!first) {
+		console.log(`[Loader] 7378278/RESTART error code found. Restarting...`);
+  }
+  var child = child_process.spawn("node main.js", [], {
+    cwd: __dirname,
+    maxBuffer: 16384 * 1024,
+    stdio: [
+      'pipe',
+      'pipe',
+      'pipe'
+    ]
+  });
+  child.on("close", (code, signal) => {
+    if ((code || signal) == 7378278) {
+      return loader(false);
+    }
+    
+    console.log(`[Loader] main.js throw ${code || signal} (not 7378278/RESTART). Shutting down...`);
+  });
+  child.on("error", function (err) {
+    console.log("[Loader] Error:", err);
+  })
+}
+loader(true);
 
-while (errorCode == 7378278) {
+/* while (errorCode == 7378278) {
 	if (!first) {
 		console.log(`[Loader] 7378278/RESTART error code found. Restarting...`);
   }
@@ -23,4 +53,4 @@ while (errorCode == 7378278) {
 }
 
 console.log();
-console.log(`[Loader] main.js throw ${errorCode} (not 7378278/RESTART). Shutting down...`);
+console.log(`[Loader] main.js throw ${errorCode} (not 7378278/RESTART). Shutting down...`); */
