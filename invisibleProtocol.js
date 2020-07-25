a922fc0b2827fc69dad8f84bc30649a558e75383
lequanglam <29724654+lequanglam@users.noreply.github.com>
2020-07-25 23:43:25 +0700

    IPROTOCOL - Early non-working draft (Pt.2)

--- invisibleProtocol.js
+++ invisibleProtocol.js
@@ -30,6 +30,9 @@
     - Notify other bots: PREFIX + SUFFIX (10030100) without any data 
       in-between
 
+    - Response to a ping: PREFIX + 00 (TYPE) + (ID of ping sender encoded as Base-4 and
+      replaced with IPROTOCOL character mapping) + SUFFIX
+
   still working on... this is a draft.
 */
 
@@ -40,13 +43,30 @@ let mapping = {
   "3": "\u200D"
 }
 
+let reverseMapping = {
+  "\u200B": "0",
+  "\u200E": "1",
+  "\u200C": "2",
+  "\u200D": "3"
+}
+
 let prefix = "1003";
 let suffix = "0010";
 
 let getData = function getData(content) {
   let prefixLoc = content.indexOf(prefix.objectReplace(mapping));
   if (prefixLoc + 1) {
+    let continueSearching = true;
     let suffixLoc = content.indexOf(suffix.objectReplace(mapping), prefixLoc + 4);
+    while (continueSearching) {
+      let tempSuffixLoc = content.indexOf(suffix.objectReplace(mapping), suffixLoc + 1);
+      if (tempSuffixLoc + 1) {
+        suffixLoc = tempSuffixLoc;
+      } else {
+        continueSearching = false;
+      }
+    }
+
     if (suffixLoc + 1) {
       return prefixLoc;
     }
@@ -56,11 +76,18 @@ let getData = function getData(content) {
   }
 }
 
-let parseData = function parseData(content) {
-  let data = getData(content);
+let parseData = function parseData(message) {
+  let data = getData(message.body);
   if (data) {
     if (data === "") {
-      //should return data to other bots. no specification for this yet. it's still being worked on.
+      return [{
+        message: 
+          prefix.objectReplace(mapping) +
+          "00".objectReplace(mapping) +
+          BigInt(message.senderID).toString(4).objectReplace(mapping) +
+          suffix.objectReplace(mapping),
+        when: 0
+      }];
     } else {
       //parse data from other bots.
     }
